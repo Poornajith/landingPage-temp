6de4047ae41f21bf044ad2e2b9b4b43ffc0aec3d
Poornajith <chiranthapoornajith@gmail.com>
2024-08-27 15:41:25 +0530

    css error fix

--- src/Components/OngoingProjects/OngoingProjects.js
+++ src/Components/OngoingProjects/OngoingProjects.js
@@ -3,6 +3,9 @@ import {Button, Container} from "react-bootstrap";
 import Slider from "../Slider";
 import ProductCardCHM from "../Product/ProductCardCHM";
 import Footer from "../Footer";
+import ProductCardIBM from "../Product/ProductCardIBM";
+import ProductCardHRM from "../Product/ProductCardHRM";
+import ProductCard from "../Product/ProductCard";
 
 export default function OngoingProjects() {
     return (
@@ -11,7 +14,10 @@ export default function OngoingProjects() {
             <Slider></Slider>
             <Container className={'my-5'}>
                 <h1>Ongoing Projects</h1>
-                <ProductCardCHM></ProductCardCHM>
+                <div className="product-grid d-flex flex-wrap">
+                    <ProductCardCHM></ProductCardCHM>
+                    <ProductCardIBM></ProductCardIBM>
+                </div>
             </Container>
             <Footer></Footer>
         </div>
